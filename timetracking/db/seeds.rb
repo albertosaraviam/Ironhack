6dab8358d86e74c6cce252eb7d3fb24f8eb74488
Alberto Saravia <albertosaraviam@gmail.com>
2016-07-05 13:33:23 -0500

    Learn seeds, create and destroy databases

--- timetracking/db/seeds.rb
+++ timetracking/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+projectA = Project.create(name: "Ironhack", description: "coding camp")
+projectB = Project.create(name: "Soccer", description: "fun times")
+projectC = Project.create(name: "Pizza Hut", description: "good pizza")
+projectD = Project.create(name: "Beer", description: "Refreshing")
+projectE = Project.create(name: "Water", description: "Healthy")
+projectF = Project.create(name: "Ice", description: "Cold")
+projectG = Project.create(name: "Whale", description: "Fat")
+projectH = Project.create(name: "Table", description: "Flat")
+projectI = Project.create(name: "Happy", description: "Today")
+
+puts "done"
