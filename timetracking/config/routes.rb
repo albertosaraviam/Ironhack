0294abc5d4db76204b672972b3d504303e98dda5
Alberto Saravia <albertosaraviam@gmail.com>
2016-07-07 13:56:53 -0500

    added a delete button

--- timetracking/config/routes.rb
+++ timetracking/config/routes.rb
@@ -27,6 +27,10 @@ Rails.application.routes.draw do
 
   patch '/projects/:project_id/time_entries/:id', to: 'time_entries#update', as: :project_time_entry
 
+  delete '/projects/:project_id/time_entries/:id', to: "time_entries#destroy"
+
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
